7884f8ee4e65670f02e6ae70dba4a29c2b6dd3a3
Alexoaner <alexander.smiljic@hotmail.com>
2015-09-26 20:42:08 +0200

    Added a log in and a database(in VS) that has one table(the user table).

--- TheATM/TheATM/Default.aspx
+++ TheATM/TheATM/Default.aspx
@@ -1,7 +1,37 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/ATM.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TheATM.Default" %>
 
 <asp:Content ContentPlaceHolderID="MainContent" ID="defaultpage" runat="server">
+    <link href="CSS/LoginStyleSheet.css" rel="stylesheet" />
     <div>
-    <p>Test</p>
+        <div id="welcome">
+            <h1>Welcome</h1>
+        </div>
+        <div id="login">
+            <table>
+                <tr>
+                    <td colspan="2" style="text-align: center; font-size: 150%">Log In</td>
+                </tr>
+                <tr>
+                    <td style="text-align: right">Card Number</td>
+                    <td>
+                        <asp:TextBox ID="TextBoxCardNumber" runat="server" EnableTheming="True"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="text-align: right">PIN</td>
+                    <td>
+                        <asp:TextBox ID="TextBoxPIN" runat="server" TextMode="Password" MaxLength="4"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="text-align: center">
+                        <asp:Button ID="LoginButton" runat="server" Text="Log In" OnClick="LoginButton_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="text-align: center"></td>
+                </tr>
+            </table>
+        </div>
     </div>
-    </asp:Content>
+</asp:Content>
--- TheATM/TheATM/Default.aspx.cs
+++ TheATM/TheATM/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace TheATM
 {
@@ -12,6 +13,42 @@ namespace TheATM
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            var connection = new SqlConnection(@"Data Source = localhost\sqlexpress;Initial Catalog=ATMDatabase;Integrated Security=SSPI");
+            connection.Open();
+            string checkCardNumber = $"select count(*) from UserTable where UserTable.CardNumber = @CardNumber";
+            var command = new SqlCommand(checkCardNumber, connection);
+            command.Parameters.Add(new SqlParameter("@CardNumber", TextBoxCardNumber.Text));
+            int temp = Convert.ToInt32(command.ExecuteScalar().ToString());
+            connection.Close();
+
+            if (temp == 1)
+            {
+                connection.Open();
+                string checkPin = $"select PIN from UserTable where UserTable.CardNumber = @CardNumber";
+                var PassCommand = new SqlCommand(checkPin, connection);
+                PassCommand.Parameters.Add(new SqlParameter("@CardNumber", TextBoxCardNumber.Text));
+                string pin = PassCommand.ExecuteScalar().ToString().Trim();
+
+                if (pin == TextBoxPIN.Text)
+                {
+                    Session["Logged"] = TextBoxCardNumber.Text; //Holds the CardNumber(PK)
+                    Response.Write("Logging in ..."); //Log in Passed
+                    //Response.Redirect("MainPage.aspx");
+                    //Server.Transfer("MainPage.aspx");
+                }
+                else
+                {
+                    Response.Write("Wrong PIN code"); //Log in Faild, wrong PIN
+                }
+            }
+            else
+            {
+                Response.Write("CCN could Not be found"); //Wrong CardNumber, not in db(not supported?)
+            }
         }
     }
 }
